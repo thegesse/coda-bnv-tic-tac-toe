1d41b91d318a07798454097e85d639df53b7f07f
Pierre caillous <Pierre.cailloupgmail.com>
2025-10-17 13:09:34 +0200

    add player 1

--- main.c
+++ main.c
@@ -17,12 +17,22 @@ void draw_grid(){
     printf("     %c  |      %c    |   %c    \n", grid[6], grid[7], grid[8]);
 
 }
-
+void machine(){
+    
+}
 int main(){
     int game_state = 0;
     draw_grid();
+    printf("player 1 is 'o', player 2 is computer and is 'x' \n");
     while(game_state == 0){
-        printf
+        int  choice;
+        printf("player 1 turn:\n ");
+        int placement = scanf("%d", &choice);
+        grid[choice] = 'o';
+        draw_grid();
+
+
+
     }
 
 }
